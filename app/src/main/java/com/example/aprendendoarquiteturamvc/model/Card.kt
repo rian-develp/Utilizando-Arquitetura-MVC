5ebb87a0d8bf6415a377bda9831e456b71badaa0
rian-develp <riandeveloper1@gmail.com>
2024-10-30 16:06:00 -0300

    Subindo a tela home e já com a tela de inserir cartão criada

---
+++ app/src/main/java/com/example/aprendendoarquiteturamvc/model/Card.kt
@@ -0,0 +1,10 @@
+package com.example.aprendendoarquiteturamvc.model
+
+data class Card(
+    val cardName: String,
+    val cardValidity: String,
+    val cardNumber: String,
+    val color: Int,
+    val customerName: String,
+    val img: String
+)
--- app/src/main/java/com/example/aprendendoarquiteturamvc/view/home/HomeActivity.kt
+++ app/src/main/java/com/example/aprendendoarquiteturamvc/view/home/HomeActivity.kt
@@ -1,24 +1,38 @@
 package com.example.aprendendoarquiteturamvc.view.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aprendendoarquiteturamvc.adapter.RecyclerAdapter
 import com.example.aprendendoarquiteturamvc.databinding.ActivityHomeBinding
+import com.example.aprendendoarquiteturamvc.model.Card
+import com.example.aprendendoarquiteturamvc.view.insertCard.InsertCardActivity
 
 class HomeActivity : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var adapter: RecyclerAdapter
+    private val list = mutableListOf<Card>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = intent
+        val recyclerViewXML = binding.recyclerView
+        recyclerViewXML.setHasFixedSize(true)
+        recyclerViewXML.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        adapter = RecyclerAdapter(this, list)
+        recyclerViewXML.adapter = adapter
 
-        val nomeUsuario = intent.getStringExtra("nomeUsuario")
-        val senhaUsuario = intent.getStringExtra("senhaUsuario")
-
-        binding.edtNome.text = "Nome do Usuário: $nomeUsuario"
-        binding.edtSenha.text = "Senha do Usuário: $senhaUsuario"
+        val floatingActionButton = binding.FAB
+        floatingActionButton.setOnClickListener{
+            val intent = Intent(this, InsertCardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
